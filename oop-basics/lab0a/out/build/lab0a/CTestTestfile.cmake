9263f698125c520112ee75a4120ff3d2180f9455
chpsdsh.fedora <bmxsib1@gmail.com>
2024-09-12 00:53:03 +0700

    added c-programming and oop-basics

---
+++ oop-basics/lab0a/out/build/lab0a/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chopisdish/Programming/Labs/lab0a
+# Build directory: /home/chopisdish/Programming/Labs/lab0a/out/build/lab0a
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
