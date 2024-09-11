83ade5d73ee7821da16d2f90975fe90b15e7fa5d
Ilya Kuznetsov <144603342+chpsdsh@users.noreply.github.com>
2024-09-12 00:54:41 +0700

    Delete oop-basics/lab0a directory

--- oop-basics/lab0a/out/build/lab0a/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chopisdish/Programming/Labs/lab0a
-# Build directory: /home/chopisdish/Programming/Labs/lab0a/out/build/lab0a
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
