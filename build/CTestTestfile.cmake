0da786e1f4826b0758cc5bf2bf7a9e533dd19d5f
chpsdsh.fedora <bmxsib1@gmail.com>
2025-02-03 13:32:22 +0700

    i love java

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chopisdish/Programming/NSU_LABS/oop-basics/lab4
+# Build directory: /home/chopisdish/Programming/NSU_LABS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
