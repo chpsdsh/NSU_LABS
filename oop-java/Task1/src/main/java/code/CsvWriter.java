d94b041ec492e8ea84066d7af38ea411bce1c4b8
chpsdsh.fedora <bmxsib1@gmail.com>
2025-02-10 12:43:42 +0700

    added columns

--- oop-java/Task1/src/main/java/code/CsvWriter.java
+++ oop-java/Task1/src/main/java/code/CsvWriter.java
@@ -9,13 +9,12 @@ public class CsvWriter {
     public static void inputDataToCsv(String outputFileName, FileData data) {
         int wordCount = data.getWordCount();
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
-            for (Map.Entry<String, Integer> entry : data.getWordMap().entrySet())
-                try {
-                    writer.write(entry.getKey() + "," + entry.getValue() + "," + (double) entry.getValue() / (double) wordCount * 100);
-                    writer.newLine();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            writer.write("Word,Frequency,Frequency(%)");
+            writer.newLine();
+            for (Map.Entry<String, Integer> entry : data.getWordMap().entrySet()) {
+                writer.write(entry.getKey() + "," + entry.getValue() + "," + (double) entry.getValue() / (double) wordCount * 100);
+                writer.newLine();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
