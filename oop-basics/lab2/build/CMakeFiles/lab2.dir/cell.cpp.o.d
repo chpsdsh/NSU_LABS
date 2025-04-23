06339f483ac19ddc1d53830eed753feda48a66df
chpsdsh.fedora <bmxsib1@gmail.com>
2025-04-23 23:33:02 +0700

    rename

--- oop-basics/lab2/build/CMakeFiles/lab2.dir/cell.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/lab2.dir/cell.cpp.o: \
- /home/chopisdish/Programming/NSU_LABS/oop-basics/lab2/cell.cpp \
- /usr/include/stdc-predef.h \
- /home/chopisdish/Programming/NSU_LABS/oop-basics/lab2/cell.h
