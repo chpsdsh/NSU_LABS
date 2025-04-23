a8bc290bbb6c19e16bb8dac641392de5103bea82
chpsdsh.fedora <bmxsib1@gmail.com>
2025-04-23 23:41:42 +0700

    rename

--- oop-basics/lab0a/out/build/lab0a/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chopisdish/Programming/NSU_LABS/oop-basics/lab0a
-# Build directory: /home/chopisdish/Programming/NSU_LABS/oop-basics/lab0a/out/build/lab0a
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
